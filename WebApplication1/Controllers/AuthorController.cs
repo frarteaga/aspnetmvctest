9f50e57de6eac05f174ab38a9a0b6e87413c3e6b
farteaga8912 <farteaga8912@gmail.com>
2016-11-23 17:36:26 -0800

    First Commit: AuthorController.Random for test

---
+++ WebApplication1/Controllers/AuthorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author/Random
+        public ActionResult Random()
+        {
+            var author = new Author() { Id = 1, Name = "Frank Arteaga" };
+            //return View(author);
+            //return HttpNotFound();
+            //return RedirectToAction("Index", "Home", new { page = "1", orderBy = "size" });
+            return File("E:\\java.png", "application/png", "p.png");
+        }
+    }
+}
